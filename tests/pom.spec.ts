bbb3c2ee3c16aa4ce3da8853496f6512c053b671
Mahmoud Mesalem <mahmoudmesalem23@gmail.com>
2025-12-03 13:45:56 +0200

    Adding fixtures and applying in POM

--- tests/pom.spec.ts
+++ tests/pom.spec.ts
@@ -1,11 +1,8 @@
-import { Expect, Page, test } from "@playwright/test";
+import { expect, test } from "../fixtures/fixture";
 import LoginPage from "./pages/loginPage/loginPage";
 import ProductPage from "./pages/productPage/productPage";
 
-test('E2E',async({page})=> {
-    const loginPage = new LoginPage(page);
-    const productPage = new ProductPage(page);
-
+test('E2E',async({page,loginPage,productPage})=> {
     await page.goto('https://www.saucedemo.com/');
     await loginPage.enterUsername();
     await loginPage.enterPassword();
