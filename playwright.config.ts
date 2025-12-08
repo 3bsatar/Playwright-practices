d1d3682a300759723494b313abfc2a3d1d19e620
Mahmoud Mesalem <mahmoudmesalem23@gmail.com>
2025-12-08 22:36:06 +0200

    Tags: runs by groups

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
   },
-
+  grep:[new RegExp('@smoke')],
   /* Configure projects for major browsers */
   projects: [
     {
